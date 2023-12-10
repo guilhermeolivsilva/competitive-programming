0d915125b5fed0a0316b0b8441153d52c6a2de35
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2023-12-10 18:54:00 -0300

    beecrowd/1002: added Java implementation

---
+++ beecrowd/1002/exercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class exercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        try {
+            float radius = Float.parseFloat(input);
+            double area = Math.PI * radius * radius;
+            System.out.printf("A=%.4f\n", area);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Not a valid number");
+        }        
+    }
+}
